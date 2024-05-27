3e7b4f70d9e85592cea81b75e3cd9193064e8b6e
Shrawan2002 <shrawankumar1754@gmail.com>
2024-05-27 10:35:19 +0530

    initial files

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -3,14 +3,27 @@ import Button from '@mui/material/Button';
 import { useState } from 'react';
 export default function SearchBox(){
     let [city,setCity] = useState("");
+    let API_URL = "https://api.openweathermap.org/data/2.5/weather";
+    let API_KEY = "bf240d3f03ef13b9a31605b016e40eac";
+    let getWeatherInfo = async()=>{
+        let response = await fetch(`${API_URL}?q=${city}$appid=${API_KEY}`);
+        let jsonResponse = await response.json();
+        console.log(jsonResponse);
+    }
     let handleChange = (event)=>{
         setCity(event.target.value);
+    }
+    let handleSubmit = (event)=>{
+        event.preventDefault();
+        console.log(city);
+        setCity("");
+        getWeatherInfo();
     }
 
     return (
         <div>
             <h3>Search for the weather</h3>
-            <form>
+            <form onSubmit={handleSubmit}>
             <TextField id="City" label="City Name" 
             variant="outlined" required  value={city} onChange={handleChange}/>
             <br></br>  <br></br>
