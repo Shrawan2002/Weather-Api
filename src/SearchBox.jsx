e0d9ae8bc2866ab465a00834d6783070a31a18d5
Shrawan2002 <shrawankumar1754@gmail.com>
2024-05-27 11:00:48 +0530

    initial files

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -6,10 +6,16 @@ export default function SearchBox(){
     let API_URL = "https://api.openweathermap.org/data/2.5/weather";
     let API_KEY = "bf240d3f03ef13b9a31605b016e40eac";
     let getWeatherInfo = async()=>{
-        let response = await fetch(`${API_URL}?q=${city}$appid=${API_KEY}`);
+        let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
         let jsonResponse = await response.json();
         console.log(jsonResponse);
+        let result = {
+            temp:jsonResponse.main.temp,
+            tempMax:jsonResponse.main.temp_max,
+            tempMin:jsonResponse.main.temp_min,
+        }
     }
+   
     let handleChange = (event)=>{
         setCity(event.target.value);
     }
